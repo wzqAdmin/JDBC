0df2faf607a6a28078a125f75293790b0f1143a5
WZQ <2720852268@qq.com>
2021-07-08 16:48:05 +0800

    hot-fix version 4.0

--- src/com/wzq/jdbc/TestJDBC.java
+++ src/com/wzq/jdbc/TestJDBC.java
@@ -2,6 +2,7 @@ package com.wzq.jdbc;
 
 public class TestJDBC {
     public static void main(String[] args) {
+        System.out.println("hello hot-fix");
         System.out.println("version 3.0");
     }
 }
