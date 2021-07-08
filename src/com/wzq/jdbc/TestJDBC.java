8792abb774323f3904ac2173d9bd7dfa239ad7c4
WZQ <2720852268@qq.com>
2021-07-08 17:02:17 +0800

    master version 5.0

--- src/com/wzq/jdbc/TestJDBC.java
+++ src/com/wzq/jdbc/TestJDBC.java
@@ -4,5 +4,6 @@ public class TestJDBC {
     public static void main(String[] args) {
         System.out.println("hello hot-fix");
         System.out.println("version 3.0");
+        System.out.println("master test!");
     }
 }
