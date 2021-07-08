db7461560c82b70df51845a099c6d0aec2a676e9
WZQ <2720852268@qq.com>
2021-07-08 16:32:10 +0800

    version 3.0

--- src/com/wzq/jdbc/TestJDBC.java
+++ src/com/wzq/jdbc/TestJDBC.java
@@ -2,6 +2,6 @@ package com.wzq.jdbc;
 
 public class TestJDBC {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("version 3.0");
     }
 }
